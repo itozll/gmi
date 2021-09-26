f657f1f1f1a53750cfa6a2c57ed34f79ecb67ef0
joiky <joiky@163.com>
2021-09-26 20:27:35 +0800

    opt glog

--- pkg/glog/default.go
+++ pkg/glog/default.go
@@ -1,12 +1,15 @@
 package glog
 
+import "sync"
+
 var (
 	dflOptions = Options{}
 	dfl        = New(&dflOptions)
+	lk         sync.RWMutex
 )
 
-func SetDefaultLog(l Logger) { dfl = l }
-func GetDefaultLog() Logger  { return dfl }
+func SetDefaultLog(l Logger) { lk.Lock(); defer lk.Unlock(); dfl = l }
+func GetDefaultLog() Logger  { lk.RLock(); defer lk.RUnlock(); return dfl }
 
 func WithMap(fields map[string]interface{}) Logger {
 	return dfl.WithMap(fields)
@@ -16,9 +19,9 @@ func WithFields(key string, val interface{}, kvs ...interface{}) Logger {
 	return dfl.WithFields(key, val, kvs...)
 }
 
-func Debug(msg string) { dfl.Debug(msg) }
-func Info(msg string)  { dfl.Info(msg) }
-func Warn(msg string)  { dfl.Warn(msg) }
-func Error(msg string) { dfl.Error(msg) }
-func Fatal(msg string) { dfl.Fatal(msg) }
-func Panic(msg string) { dfl.Panic(msg) }
+func Debug(msg string) { GetDefaultLog().Debug(msg) }
+func Info(msg string)  { GetDefaultLog().Info(msg) }
+func Warn(msg string)  { GetDefaultLog().Warn(msg) }
+func Error(msg string) { GetDefaultLog().Error(msg) }
+func Fatal(msg string) { GetDefaultLog().Fatal(msg) }
+func Panic(msg string) { GetDefaultLog().Panic(msg) }
